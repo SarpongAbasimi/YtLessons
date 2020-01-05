897f55273811c0cc8b1c2261100cc4c981a3c829
sarpong <abasimisarpong@rocketmail.com>
2020-01-05 10:58:16 +0000

    Added functions to App 🤓

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import './App.css';
+import  { Input } from './components/input';
+import { ToDoList } from './components/todoList';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [todos, setTodos] = useState(''),
+        [listOfToDos, setListOfToDos] = useState([]);
+
+  const handleSubmitToDo = (userToDo) => {
+    setListOfToDos(listOfToDos.concat(userToDo))
+  };
+
   return (
     <div className="App">
-
+      <Input initailToDo={todos} onSubmitToDo={handleSubmitToDo} setTodo={setTodos}/>
+      <ToDoList toDoList={listOfToDos}/>
     </div>
   );
 }
