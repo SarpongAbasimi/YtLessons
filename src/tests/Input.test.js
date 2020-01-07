222525b84dbed27a755d433c24a9df973d591f89
sarpong <abasimisarpong@rocketmail.com>
2020-01-07 20:39:52 +0000

    input test

--- src/tests/Input.test.js
+++ src/tests/Input.test.js
@@ -2,8 +2,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Input } from '../components/Input';
 
-describe('', ()=> {
-  it('says hello', () => {
+describe('<Input />', ()=> {
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = shallow(< Input/>);
+  });
+
+  it('It has an input field', () => {
+    const input = wrapper.find('.inputField')
+    expect(input.props().value).toBe(undefined)
+  });
+  
+  it('contains a button', () => {
+    const button = <button> submit todo </button>
+    expect(wrapper.containsMatchingElement(button)).toBe(true);
+  });
+
+  it('There is a text on the button', () => {
+    const button = wrapper.find('.button');
+    expect(button.text()).toEqual('submit todo');
   });
 });
